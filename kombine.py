9b95186b74f7e4c55ecd6c74c2a7317605125e0a
Amyth Singh <aroras.official@gmail.com>
2016-04-12 11:17:39 +0530

    Some minor fixes

--- kombine.py
+++ kombine.py
@@ -8,6 +8,7 @@ import re
 import sys
 import syslog
 import time
+from logging.handlers import RotatingFileHandler
 
 from pygtail import Pygtail
 
@@ -17,11 +18,12 @@ class LogEntry(object):
     def __init__(self):
         self.queue_id = ""
         self.from_email = ""
-        self.recipient = ""
-        self.mailer_tag = ""
+        self.email= ""
+        self.campaign = ""
         self.status = ""
         self.initiated_at = ""
         self.sent_at = ""
+        self.campdt = ""
 
 
 class Kombiner(object):
@@ -31,20 +33,22 @@ class Kombiner(object):
     """
 
     def __init__(self, input_file, output_file=None, interval=5,
-            max_size=1024 * 1024 * 2):
+            max_size=1024 * 1024 * 2000, backup_count=7, file_handler=None):
 
         self.input_file = input_file
         self.output_file = output_file
         self.interval = interval
-        self.identifier = r'\w{3}\s+\d+\s+\d+:\d{2}:\d{2}\s\w+\-\w+\s\w+/\w+\[\d+\]:\s\w+'
-        self.queue_regex = r'^\w{3}\s\d{2}\s\d{2}:\d{2}:\d{2}\s\w+\-\w+\s\w+/\w+\[\d+\]:\s(\w+)'
+        self.identifier = r'\w{3}\s+\d+\s+\d+:\d{2}:\d{2}\s[\w\d\/\-\.]+\s[\w\d\/\-\.]+\[\d+\]:\s\w+'
+        self.queue_regex = r'^\w{3}\s+\d+\s+\d+:\d{2}:\d{2}\s[\w\d\/\-\.]+\s[\w\d\/\-\.]+\[\d+\]:\s(\w+)'
         self.from_regex = r'from=<([\w\d@.-]+)'
         self.to_regex = r'to=<([\w\d@.-]+)'
         self.mailer_tag_regex = r'X-MailerTag:\s([\w\d]+)'
         self.status_regex = r'status=([\w\d]+)'
-        self.date_regex = r'^(\w{3}\s\d{2}\s\d{2}:\d{2}:\d{2})'
+        self.date_regex = r'^(\w{3}\s+\d+\s+\d+:\d{2}:\d{2})'
         self.max_size = max_size
+        self.backup_count = backup_count
         self.entries = {}
+        self.file_handler = file_handler
 
         ## Set default output file
         if self.output_file is None:
@@ -67,6 +71,9 @@ class Kombiner(object):
             syslog.syslog(syslog.LOG_ERR, "KOMBINE: could not create file "\
                     "make sure the output folder has the correct owner and "\
                     "permissions.")
+        if not self.file_handler:
+            self.file_handler = RotatingFileHandler(self.output_file,
+                    maxBytes=self.max_size, backupCount=self.backup_count)
 
     def kombine(self):
         """
@@ -110,7 +117,9 @@ class Kombiner(object):
 
                 self.parse_line(obj, line)
             except Exception as err:
-                syslog.syslog(syslog.LOG_ERR, "KOMBINE: %s" % str(err))
+                syslog.syslog(syslog.LOG_ERR, "KOMBINE: %s : %s" % (str(err), line))
+        else:
+            syslog.syslog(syslog.LOG_ERR, "KOMBINE Unmatched Postfix: %s" % line)
 
     def get_mailer_tag(self, line):
         """
@@ -128,13 +137,13 @@ class Kombiner(object):
         """
         Logs to the output file and removes the object from memory
         """
-        if (hasattr(obj, 'recipient') and obj.recipient) and \
+        if (hasattr(obj, 'email') and obj.email) and \
                 (hasattr(obj,'from_email') and obj.from_email):
+            campaign_date = datetime.datetime.strptime(obj.initiated_at + " " + str(datetime.datetime.today().year), "%b %d %H:%M:%S %Y")
+            obj.campdt = campaign_date.strftime("%Y%m%d")
             json_string = json.dumps(obj.__dict__)
-            with open(self.output_file, "a") as output_file:
-                output_file.write(json_string + "\n")
+            self.file_handler.stream.write(json_string + "\n")
         del self.entries[obj.queue_id]
-        self.rotate_file()
 
     def parse_line(self, obj, line):
         """
@@ -146,9 +155,9 @@ class Kombiner(object):
 
         elif "X-MailerTag" in line:
             tag, frm, to = self.get_mailer_tag(line)
-            obj.mailer_tag = tag
+            obj.campaign = tag
             obj.from_email = frm
-            obj.recipient = to
+            obj.email = to
             self.entries[obj.queue_id] = obj
 
         elif "status=" in line:
@@ -163,28 +172,6 @@ class Kombiner(object):
             obj.status = "denied"
             self.entries[obj.queue_id] = obj
             self.log_entry(obj)
-
-    def rotate_file(self):
-        if os.path.getsize(self.output_file) >= self.max_size:
-            now = datetime.datetime.now().toordinal()
-            output = open(self.output_file, "r")
-            zipped = gzip.open("%s.%s.gz" % (self.output_file, now), "wb")
-            zipped.writelines(output)
-            output.close()
-            zipped.close()
-
-            ## empty the contents of the output file
-            with open(self.output_file, "w") as empty:
-                pass
-
-            try:
-                ## resets the pygtail offset so we can read
-                ## the file again from the top as it has been
-                ## reset.
-                os.remove("%s.offset" % self.input_file)
-            except Exception as err:
-                pass
-
 
 
 if __name__ == '__main__':
